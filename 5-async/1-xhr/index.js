7ee4c49005c8aec44e6882fb4b7b331d721fb929
liaowenqiang828 <18840965351@163.com>
2020-08-10 23:09:01 +0800

    [Wenqiang Liao] feat: finish 5-async of 1-xhr

--- 5-async/1-xhr/index.js
+++ 5-async/1-xhr/index.js
@@ -2,7 +2,18 @@ function fetchData(url, successCallback, errorCallback) {
   const xhr = new XMLHttpRequest();
   // <-- start
   // TODO 21: 通过XMLHttpRequest实现异步请求
+  xhr.open('GET', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status <= 299) {
+      successCallback(xhr);
+    }
+    if (xhr.readyState === 4 && (xhr.status < 200 || xhr.status > 299)) {
+      errorCallback(xhr);
+    }
+  };
 
+  xhr.send();
   // end -->
 }
 
@@ -10,7 +21,7 @@ const URL = 'http://localhost:3000/api';
 fetchData(
   URL,
   result => {
-    document.writeln(JSON.parse(result).name);
+    document.writeln(JSON.parse(result.responseText).name);
   },
   error => {
     console.error(error);
