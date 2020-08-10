4bef2a03940250e898a72ee8f09fb7191b3b37a8
liaowenqiang828 <18840965351@163.com>
2020-08-10 19:52:51 +0800

    [Wenqiang Liao] feat: finish 2-array of find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,6 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const person = collection.find(person => person.age <= 20);
+  const { name } = person;
+  return name;
 }
