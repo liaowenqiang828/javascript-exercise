67566fec3dec066c83055d132d7cc7bf4c92e244
liaowenqiang828 <18840965351@163.com>
2020-08-10 19:45:06 +0800

    [Wenqiang Liao] feat: finish 2-array of filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,13 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const resultArray = collection.filter(number => number % 3 === 0);
+  return resultArray;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const resultArray = collection.filter((number, index) => collection.indexOf(number) === index);
+  return resultArray;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
