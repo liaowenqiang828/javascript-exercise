d66a3dda89a81fd6a9572d189c99b54ca0fbe2d5
liaowenqiang828 <18840965351@163.com>
2020-08-10 20:04:23 +0800

    [Wenqiang Liao] feat: finish 3-object of assign

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,6 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  source.serialNumber = '12345';
+  source.properties.status = 'processed';
+  return source;
 }
