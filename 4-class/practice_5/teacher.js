96a760a2df9b3f9b1b1439e50910a0b191e424cc
liaowenqiang828 <18840965351@163.com>
2020-08-10 21:25:33 +0800

    [Wenqiang Liao] feat: finish 4-class of practice_4 and practice_5

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,15 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+
+class Teacher extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+
+  introduce() {
+    const classInfo = this.klass ? `Class ${this.klass}` : 'No Class';
+    return `${super.introduce()} I am a Teacher. I teach ${classInfo}.`;
+  }
+}
+export default Teacher;
