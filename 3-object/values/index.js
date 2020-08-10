8c9ec79799952b81ea953ce82a50a4bf3821e090
liaowenqiang828 <18840965351@163.com>
2020-08-10 20:13:43 +0800

    [Wenqiang Liao] feat: finish 3-object of values

--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  let count = 0;
+  Object.values(source).forEach(value => (count += parseInt(value)));
+  return count;
 }
