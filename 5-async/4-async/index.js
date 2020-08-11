e05a92ebe8081e450a9e731a246bae749022e8fe
liaowenqiang828 <18840965351@163.com>
2020-08-11 08:50:54 +0800

    [Wenqiang Liao] feat: finish 5-async of 4-xhr

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -1,7 +1,11 @@
 async function fetchData(url) {
   // <-- start
   // TODO 24: 通过await/async实现异步请求
-  // end -->
+  await fetch(url)
+    .then(result => {
+      return result.json();
+    })
+    .then(data => document.writeln(data.name));
 }
 
 const URL = 'http://localhost:3000/api';
